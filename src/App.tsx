dcf43600823f57f070a20375a689e6b24147b12e
Bineesh-pro <bineeshprofessional@gmail.com>
2024-08-21 22:58:20 +0530

    first commit-project initial setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import {Button, TextField} from "@mui/material";
+
+function App() {
+
+  return (
+      <div>
+          <div>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <Button variant="contained">Hello World</Button>
+          </div>
+
+          <div>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <TextField id="outlined-basic" label="Bineesh"></TextField>
+              <Button variant="contained">Hello World</Button>
+          </div>
+      </div>
+
+
+  )
+}
+
+export default App
