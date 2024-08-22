a940844bd364d60201bc5513af408a87f0700e4e
Bineesh-pro <bineeshprofessional@gmail.com>
2024-08-22 19:51:19 +0530

    complete ui implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,52 @@
 
 import './App.css'
-import {Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField} from "@mui/material";
+import {
+    Button,
+    styled,
+    Table,
+    TableBody,
+    TableCell, tableCellClasses,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from "@mui/material";
 
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-) {
-    return { name, calories, fat, carbs, protein };
-}
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#2672ff",
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+const StyledTableRow = styled(TableRow)(() => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: "#e5e9fe",
+    }
+}));
+
+const courseList = [
+    { "title" : "Course Title 1","code" : 123098},
+    { "title" : "Course Title 2","code" : 133983},
+    { "title" : "Course Title 3","code" : 901556},
+    { "title" : "Course Title 4","code" : 407522},
 ];
+
+const courseInstance = [
+    { "title" : "Course Title 1","year-sem" : "2023 - 1", "code" : 133983},
+    { "title" : "Course Title 2","year-sem" : "2023 - 1", "code" : 133983},
+    { "title" : "Course Title 3","year-sem" : "2023 - 1", "code" : 133983},
+    { "title" : "Course Title 4","year-sem" : "2023 - 1", "code" : 133983},
+    { "title" : "Course Title 5","year-sem" : "2023 - 1", "code" : 133983},
+]
 
 function App() {
 
   return (
-      <div>
+      <div className="outer-outer">
           <div className="card-outer">
               <div className="card">
                   <TextField className="tbox" id="outlined-basic" label="Course title"></TextField>
@@ -41,80 +64,111 @@ function App() {
                           <option value="DPCO">DPCO</option>
                           <option value="C programming">C programming</option>
                       </select>
-                          <Button className="sel" variant="contained">Refresh</Button>
+                      <Button className="sel" variant="contained">Referesh</Button>
                   </div>
-                      <div className="card">
-                          <div className="join-col">
-                              <TextField className="tbox" id="outlined-basic" label="Year"></TextField>
-                              <TextField className="tbox" id="outlined-basic" label="Semester"></TextField>
-                          </div>
-
-                          <Button className="but" variant="contained">Add Course</Button>
+                  <div className="card">
+                      <div className="join-col">
+                          <TextField className="tbox" id="outlined-basic" label="Year"></TextField>
+                          <TextField className="tbox" id="outlined-basic" label="Semester"></TextField>
                       </div>
-                  </div>
 
+                      <Button className="but" variant="contained">Add Course</Button>
+                  </div>
               </div>
 
+          </div>
 
+
+        <div>
+          <Button className="but" variant="contained">List Courses</Button>
+            <br/>
+            <br/>
           <TableContainer>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>Course Title</TableCell>
-                          <TableCell align="right">Code</TableCell>
-                          <TableCell align="right">Action</TableCell>
+              <Table sx={{minWidth: 700}} aria-label="customized table">
+                  <TableHead className="darkbg">
+                      <TableRow className="darkbg">
+                          <StyledTableCell>Course Title</StyledTableCell>
+                          <StyledTableCell align="center">Course</StyledTableCell>
+                          <StyledTableCell align="center">Action</StyledTableCell>
                       </TableRow>
                   </TableHead>
                   <TableBody>
-                      {rows.map((row) => (
-                          <TableRow
-                              key={row.name}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                              <TableCell component="th" scope="row">
-                                  {row.name}
-                              </TableCell>
-                              <TableCell align="right">{row.calories}</TableCell>
-                              <TableCell align="right">{row.fat}</TableCell>
-                          </TableRow>
+                      {courseList.map((row) => (
+                          <StyledTableRow key={row.title}>
+                              <StyledTableCell component="th" scope="row">
+                                  {row.title}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">{row.code}</StyledTableCell>
+                              <StyledTableCell align="center">
+
+                                  <div className="img-split">
+                                      <img src="/src/assets/bin.png" alt=""></img>
+                                      <img src="/src/assets/search.png" alt=""></img>
+                                  </div>
+
+                              </StyledTableCell>
+                          </StyledTableRow>
                       ))}
                   </TableBody>
               </Table>
           </TableContainer>
 
-          <br></br>
-          <br></br>
-          <br></br>
+        </div>
 
-          <TableContainer>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>Course Title</TableCell>
-                          <TableCell>Year sem</TableCell>
-                          <TableCell align="right">Code</TableCell>
-                          <TableCell align="right">Action</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                      {rows.map((row) => (
-                          <TableRow
-                              key={row.name}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                              <TableCell component="th" scope="row">
-                                  {row.name}
-                              </TableCell>
-                              <TableCell align="right">{row.calories}</TableCell>
-                              <TableCell align="right">{row.fat}</TableCell>
-                              <TableCell align="right">{row.fat}</TableCell>
+          <div>
+
+              <div className="inner-join">
+                  <TextField className="tbox" id="outlined-basic" label="Year"></TextField>
+                  <select className="sel">
+                      <option value="default">Select semester</option>
+                      <option value="Bio Maths">Bio Maths</option>
+                      <option value="MicroProcessor">MicroProcessor</option>
+                      <option value="DPCO">DPCO</option>
+                      <option value="C programming">C programming</option>
+                  </select>
+
+                  <Button className="but" variant="contained">List instances</Button>
+
+              </div>
+              <br/>
+              <br/>
+
+              <TableContainer>
+                  <Table sx={{minWidth: 700}} aria-label="customized table">
+                      <TableHead className="darkbg">
+                          <TableRow className="darkbg">
+                              <StyledTableCell>Course Title</StyledTableCell>
+                              <StyledTableCell align="center">Year - Sem</StyledTableCell>
+                              <StyledTableCell align="center">Code</StyledTableCell>
+                              <StyledTableCell align="center">Action</StyledTableCell>
                           </TableRow>
-                      ))}
-                  </TableBody>
-              </Table>
-          </TableContainer>
+                      </TableHead>
+                      <TableBody>
+                          {courseInstance.map((row) => (
+                              <StyledTableRow key={row.title}>
+                                  <StyledTableCell component="th" scope="row">
+                                      {row.title}
+                                  </StyledTableCell>
+                                  <StyledTableCell align="center">{row["year-sem"]}</StyledTableCell>
+                                  <StyledTableCell align="center">{row.code}</StyledTableCell>
+                                  <StyledTableCell align="center">
+
+                                      <div className="img-split">
+                                          <img src="/src/assets/bin.png" alt=""></img>
+                                          <img src="/src/assets/search.png" alt=""></img>
+                                      </div>
+
+                                  </StyledTableCell>
+                              </StyledTableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
+              </TableContainer>
 
           </div>
+
+
+      </div>
 
 
   )
